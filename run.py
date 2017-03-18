32a049e1e127539184d63d69b6bbd8f0ff595bd9
Quoc Lieu <qlieu@student.unimelb.edu.au>
2017-03-19 00:53:39 +1100

    prep merge

--- run.py
+++ run.py
@@ -1,14 +1,8 @@
 from flask import Flask, render_template, session
 app = Flask(__name__, static_folder='.', static_url_path='')
 WTF_CSRF_ENABLED = True
-from gevent import monkey
-#from socketio.server import SocketIOServer
-
-
 #import all routes from routes directory
 from routes import *
-
-monkey.patch_all()
 
 if __name__ == "__main__":
     app.run(debug=True, port=2526)
