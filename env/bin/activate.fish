b9c895caa4b95ffc3308f3d7c53f68469c0ec754
Quoc Lieu <qlieu@student.unimelb.edu.au>
2017-03-19 05:07:51 +1100

    working now

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelavent variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/Quoc/Desktop/Flask-SocketIO-Chat/env"
+set -gx VIRTUAL_ENV "/Users/Quoc/Desktop/codebrew17-starving/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
